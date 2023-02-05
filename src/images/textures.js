7ec5daf2c1a6a3a2d423ed57146a6bf2cb8f40ee
Prodigy343 <peeddurple@gmail.com>
2023-02-05 17:11:13 -0300

    adding basic logic for texture and player movement

---
+++ src/images/textures.js
@@ -0,0 +1,23 @@
+import { TextureLoader } from 'three'
+
+import {
+    dirtImg,
+    logImg,
+    grassImg,
+    glassImg,
+    woodImg
+} from './images'
+
+const dirtTexture = new TextureLoader().load(dirtImg)
+const logTexture = new TextureLoader().load(logImg)
+const grassTexture = new TextureLoader().load(grassImg)
+const glassTexture = new TextureLoader().load(glassImg)
+const woodTexture = new TextureLoader().load(woodImg)
+
+export {
+    dirtTexture,
+    logTexture,
+    grassTexture,
+    glassTexture,
+    woodTexture
+}
